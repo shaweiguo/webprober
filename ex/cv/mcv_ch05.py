25ad5eb7b4690c46636e5e08fd28cd74d60aff0d
shaweiguo <shaweiguo@yeah.net>
2023-12-22 17:10:07 +0800

    ...

--- ex/cv/mcv_ch05.py
+++ ex/cv/mcv_ch05.py
@@ -1,4 +1,6 @@
+import os
 import cv2
+import numpy as np
 
 
 def simple_threshold(img):
@@ -27,13 +29,67 @@ def otsu_threshold(img):
     cv2.imshow('OTSU Threshold Image', thresh)
 
 
+def template_matching():
+    img_path = "/home/sha/books/Hands-on-ML-Projects-with-OpenCV/data"
+    dog_img_path = os.path.join(img_path, "dog.jpg")
+    dog_head_img_path = os.path.join(img_path, "dog-head.jpg")
+
+    main_image = cv2.imread(dog_img_path)
+    template_image = cv2.imread(dog_head_img_path)
+
+    template_height, template_width, _ = template_image.shape
+    result = cv2.matchTemplate(main_image,
+                               template_image,
+                               cv2.TM_CCOEFF_NORMED)
+
+    threshold = 0.8
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+
+    for loc in locations:
+        top_left = loc
+        bottom_right = (top_left[0] + template_width,
+                        top_left[1] + template_height)
+        cv2.rectangle(main_image, top_left, bottom_right, (0, 0, 255), 1)
+
+    cv2.imshow("Result", main_image)
+
+
+def template_matching1():
+    img_path = "/home/sha/books/Hands-on-ML-Projects-with-OpenCV/data"
+    dog_img_path = os.path.join(img_path, "dog.jpg")
+    dog_head_img_path = os.path.join(img_path, "dog-head.jpg")
+
+    main_image = cv2.imread(dog_img_path, 0)
+    main_image_copy = main_image.copy()
+    template_image = cv2.imread(dog_head_img_path, 0)
+
+    template_height, template_width, _ = template_image.shape
+    result = cv2.matchTemplate(main_image,
+                               template_image,
+                               cv2.TM_CCOEFF_NORMED)
+
+    threshold = 0.8
+    locations = np.where(result >= threshold)
+    locations = list(zip(*locations[::-1]))
+
+    for loc in locations:
+        top_left = loc
+        bottom_right = (top_left[0] + template_width,
+                        top_left[1] + template_height)
+        cv2.rectangle(main_image, top_left, bottom_right, (0, 0, 255), 1)
+
+    cv2.imshow("Result", main_image)
+
+
 def main():
-    img_path = "/home/sha/Downloads/star.jpg"
+    # img_path = "/home/sha/Downloads/star.jpg"
     # img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-    img = cv2.imread(img_path, 0)
+    # img = cv2.imread(img_path, 0)
     # simple_threshold(img)
     # adaptive_threshold(img)
-    otsu_threshold(img)
+    # otsu_threshold(img)
+    template_matching()
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
